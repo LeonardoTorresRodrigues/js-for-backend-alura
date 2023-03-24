aef2067f020de36c32074a1297cb69c1f697e21a
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-03-24 12:43:44 -0300

    feat: testing string type

---
+++ type-string/type-string.js
@@ -0,0 +1,15 @@
+const texto1 = "Olá, mundo!";
+const texto2 = 'Olá, mundo!';
+const senha = "senhaSuperSegura456!";
+const stringDeNumeros = "34567";
+
+// usando "" e '' no mesmo String
+const citacao = 'O Leo disse "oi!"';
+console.log(citacao);
+
+// concatenação (+)
+const nome = "Leonardo";
+const frase = "Meu nome é ";
+console.log(frase + nome);
+
+// template string ou template literal
