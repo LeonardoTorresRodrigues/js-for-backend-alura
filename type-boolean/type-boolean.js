cce191a972f5b1db4c3acee306f67f82eb734e48
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-03-24 13:06:10 -0300

    feat: testing boolean type

---
+++ type-boolean/type-boolean.js
@@ -0,0 +1,13 @@
+const num1 = 5;
+const num2 = 10;
+const num3 = 5;
+
+console.log(num1 === num2);
+console.log(num1 === num3);
+
+let cadastroAtivado = true;
+
+const text1 = 'Alura';
+const text2 = 'alura';
+
+console.log(text1 === text2);
