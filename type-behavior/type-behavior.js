d6714412cb4be5840085629884de7425d1db6c64
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-04-07 13:43:58 -0300

    refactor: add console logs

---
+++ type-behavior/type-behavior.js
@@ -0,0 +1,9 @@
+// No caso de números, é possível salvar em uma variável não somente o número em si, mas também o resultado de uma operação.
+
+const soma = 10 + 10;
+const multiplicacao = 10 * 10;
+const operacao = (soma + multiplicacao) + 10; // utilizando parênteses () para seguir a regra matemática de precedência nas operações.
+
+console.log(soma);
+console.log(multiplicacao);
+console.log(operacao);
