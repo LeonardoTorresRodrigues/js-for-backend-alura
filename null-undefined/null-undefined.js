44b7ef62da41a0a4a1228c1e5dc5bbb82de3719a
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-04-11 11:53:06 -0300

    feat: testing null and undefined types

---
+++ null-undefined/null-undefined.js
@@ -0,0 +1,17 @@
+// undefined => não definido
+
+// null => vazio ou nada
+
+let minhaVar; // undefined
+let varNull = null; // null é criado como objeto
+
+console.log(minhaVar);
+console.log(varNull);
+
+let numero = 3;
+let texto = "alura";
+
+console.log(typeof numero);
+console.log(typeof texto);
+console.log(typeof minhaVar);
+console.log(typeof varNull);
