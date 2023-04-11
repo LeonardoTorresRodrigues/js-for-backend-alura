f1f817fd359763e9101f38c067372c77a438bfe4
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-04-11 11:53:42 -0300

    feat: testing truthy and falsy

---
+++ truthy-falsy/truthy-falsy.js
@@ -0,0 +1,11 @@
+// boolean 
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy ou falsy
+// 0 => false (é um falsy)
+console.log(0 == false);
+console.log("" == false);
+
+// 1 => true (é um truthy)
+console.log(1 == true);
