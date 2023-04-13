9c370224d8c0e383b668aa985c04fbb05d5c563f
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-04-13 11:02:04 -0300

    feat: ternary

---
+++ ternario/ternario.js
@@ -0,0 +1,12 @@
+const idadeMinima = 18;
+const idadeCliente = 18;
+
+// estrutura de comparação com if
+// if (idadeCliente >= idadeMinima) {
+//   console.log("cerveja");
+// } else {
+//   console.log("suco");
+// }
+
+// estrutura de comparação com ternário
+console.log(idadeCliente >= idadeMinima ? "cerveja" : "suco");
