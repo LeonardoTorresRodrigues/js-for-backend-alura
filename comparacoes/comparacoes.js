1adcd1a5f5537f88ef4037d2c7617f6365664593
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-04-13 10:46:33 -0300

    feat: comparison operators

---
+++ comparacoes/comparacoes.js
@@ -0,0 +1,10 @@
+// == (comparação implícita)
+const numero = 5;
+const texto = "5";
+console.log(numero === texto);
+// == true
+// === false
+
+// typeof
+console.log(typeof numero);
+console.log(typeof texto);
