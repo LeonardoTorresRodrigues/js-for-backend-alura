985968c017eff26867111abe92ae28d1800f6fb3
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-04-13 11:18:35 -0300

    feat: template string

---
+++ template-strings/template-strings.js
@@ -0,0 +1,5 @@
+const nome = "Leo";
+const idade = 2023 - 1997;
+const cidade = "São Paulo";
+const apresentacao = `Meu nome é ${nome}, minha idade é ${idade} e sou da cidade de ${cidade}`;
+console.log(apresentacao);
