76aced00a95998052dfdfef6466a4138412c7598
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-04-14 13:35:08 -0300

    feat: function expressions

---
+++ expressoes-funcao/expressoes-funcao.js
@@ -0,0 +1,10 @@
+// declaração de função
+function minhaFuncao(param) {
+  return 
+}
+
+// expressão de função
+const soma = function(num1, num2) {
+  return num1 + num2;
+}
+console.log(soma(1, 1));
