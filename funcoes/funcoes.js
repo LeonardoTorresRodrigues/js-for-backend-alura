a6dd36060bc8228db96bfd8dc6112c63048faf6d
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-04-14 12:58:47 -0300

    feat: functions

---
+++ funcoes/funcoes.js
@@ -0,0 +1,19 @@
+// let x = "";
+// console.log(x);
+// x = "oi";
+
+// declarando a função
+function imprimeTexto(texto) {
+  console.log(texto)
+}
+
+// executando a função
+imprimeTexto("oi");
+imprimeTexto("outro texto");
+imprimeTexto(soma());
+
+function soma() {
+  return 2 + 2;
+}
+
+//console.log(soma());
