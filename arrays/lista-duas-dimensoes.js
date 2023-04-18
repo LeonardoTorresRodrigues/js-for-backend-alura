38108598c0f5b6f5f5e34fb1b978ca37dbfec5b9
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-04-18 11:18:39 -0300

    feat: challenge with two arrays

---
+++ arrays/lista-duas-dimensoes.js
@@ -0,0 +1,11 @@
+const alunos = ['João', 'Juliana', 'Ana', 'Caio'];
+const medias = [10, 8, 7.5, 9];
+
+const listaDeAlunosEMedias = [alunos, medias];
+
+// console.log(listaDeAlunosEMedias);
+
+console.log(`A aluna da posição 1 da lista de alunos é: ${listaDeAlunosEMedias[0][1]}. 
+A nota dessa aluna é ${listaDeAlunosEMedias[1][1]}`);
+// acessa índice 0 do array listaDeAlunosEMedias e depois o índice 1 em alunos
+// acessa índice 1 do array listaDeAlunosEMedias e depois o índice 1 em medias
