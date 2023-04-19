f88800aad9745192352022a8c28884096a9a892c
Leonardo Rodrigues <leonardo.t.rodri@gmail.com>
2023-04-19 10:47:03 -0300

    refactor: destructuring functions

--- arrays/procurando-aluno.js
+++ arrays/procurando-aluno.js
@@ -5,16 +5,19 @@ const listaDeAlunosEMedias = [alunos, medias];
 
 function exibeNomeENota(aluno) {
   if (listaDeAlunosEMedias[0].includes(aluno)) {
+    // const alunos = listaDeAlunosEMedias[0];
+    // const medias = listaDeAlunosEMedias[1];
+    const [alunos, medias] = listaDeAlunosEMedias;
 
-    const indice = listaDeAlunosEMedias[0].indexOf(aluno);
+    const indice = alunos.indexOf(aluno);
 
-    const mediaDoAluno = listaDeAlunosEMedias[1][indice];
+    const mediaDoAluno = medias[indice];
 
     console.log(`${aluno} tem a média ${mediaDoAluno}`);
 
   } else {
-    console.log("Aluno não encontrado!")
+    console.log("Aluno não encontrado!");
   }
 }
 
-exibeNomeENota("Leo");
+exibeNomeENota("Ana");
